60c0569316d136d6ed3ff5295c027a145ae3b22f
Andre Fonseca <decobf15@gmail.com>
2024-04-10 17:33:00 -0300

    treating errors and MatDialog

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,7 +1,10 @@
-import { CoursesService } from './../services/courses.service';
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { catchError, Observable, of } from 'rxjs';
+
+import { ErrorDialogComponent } from '../../shared/components/error-dialog/error-dialog.component';
 import { Course } from '../model/course';
-import { Observable } from 'rxjs';
+import { CoursesService } from './../services/courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -15,10 +18,23 @@ export class CoursesComponent {
 
   // coursesService: CoursesService;
 
-  constructor(private coursesService: CoursesService){
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog){
   // this.courses = [];
   // this.coursesService = new CoursesService();
-  this.courses$ = this.coursesService.list();
+  this.courses$ = this.coursesService.list()
+  .pipe(
+    catchError(error => {
+      this.onError('Erro ao carregar cursos.');
+      return of([])
+    })
+  );
   }
 
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg,
+    });
+  }
 }
